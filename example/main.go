528cf3bebb12e25d5533a5a3f1e9908184f74c71
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-06 01:46:40 +0400

    test disposable

--- example/main.go
+++ example/main.go
@@ -15,4 +15,12 @@ func main() {
 	fmt.Printf("Full name: %s\n", employee.Fullname)
 	fmt.Printf("Position: %s\n", employee.Position)
 	fmt.Printf("Salary: %d\n", employee.Salary)
+
+	p1, _ := c.GetPerson()
+	p2, _ := c.GetPerson()
+	e1, _ := c.GetEmployee()
+	e2, _ := c.GetEmployee()
+
+	fmt.Printf("person is disposable, p1 == p2 %v\n", p1 == p2)
+	fmt.Printf("employee isn't disposable, e1 == e2 %v\n", e1 == e2)
 }
