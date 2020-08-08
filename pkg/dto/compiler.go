6f2d7447286fcd0fdf81e5d483ca8b3f0a17fbd3
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-08 05:30:19 +0400

    compiler

--- pkg/dto/compiler.go
+++ pkg/dto/compiler.go
@@ -1,8 +1,10 @@
 package dto
 
 import (
+	"github.com/gomponents/gontainer/pkg/arguments"
 	"github.com/gomponents/gontainer/pkg/imports"
 	"github.com/gomponents/gontainer/pkg/parameters"
+	"github.com/gomponents/gontainer/pkg/syntax"
 )
 
 type Compiler interface {
@@ -14,6 +16,8 @@ type BaseCompiler struct {
 	validator         Validator
 	compiledValidator CompiledValidator
 	paramsResolver    parameters.BagFactory
+	typeResolver      syntax.TypeResolver
+	argumentResolver  arguments.Resolver
 }
 
 func (c *BaseCompiler) Compile(i Input) (CompiledInput, error) {
@@ -30,6 +34,25 @@ func (c *BaseCompiler) Compile(i Input) (CompiledInput, error) {
 		return CompiledInput{}, paramsErr
 	}
 	r.Params = params
+
+	for n, s := range i.Services {
+		t, tErr := c.typeResolver.ResolveType(s.Type)
+		if tErr != nil {
+			return CompiledInput{}, tErr
+		}
+
+		// TODO resolve args
+
+		r.Services[n] = CompiledService{
+			Getter:      s.Getter,
+			Type:        t,
+			Constructor: s.Constructor,
+			WithError:   s.WithError,
+			Disposable:  s.Disposable,
+			Args:        nil,
+			Tags:        append(s.Args),
+		}
+	}
 
 	if err := c.compiledValidator.Validate(r); err != nil {
 		return CompiledInput{}, err
