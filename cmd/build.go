84ccc080f4704d444a7e2abc827b9ebc5b85903e
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-11 00:53:19 +0400

    improvements

--- cmd/build.go
+++ cmd/build.go
@@ -2,17 +2,12 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/gomponents/gontainer/pkg"
-	"github.com/gomponents/gontainer/pkg/template2"
 	"io/ioutil"
+	"os"
 
-	"github.com/gomponents/gontainer/pkg/arguments"
-	"github.com/gomponents/gontainer/pkg/dto"
-	"github.com/gomponents/gontainer/pkg/exporters"
+	"github.com/gomponents/gontainer/pkg"
 	"github.com/gomponents/gontainer/pkg/imports"
-	"github.com/gomponents/gontainer/pkg/parameters"
-	"github.com/gomponents/gontainer/pkg/syntax"
-	"github.com/gomponents/gontainer/pkg/tokens"
+	"github.com/gomponents/gontainer/pkg/template2"
 	"github.com/spf13/cobra"
 )
 
@@ -23,12 +18,20 @@ func NewBuildCmd() *cobra.Command {
 		cmd        *cobra.Command
 	)
 
+	writeErr := func(s string) {
+		_, err := cmd.OutOrStderr().Write([]byte(s))
+		if err != nil {
+			panic(err)
+		}
+		os.Exit(1)
+	}
+
 	handleErr := func(err error, msg ...string) {
 		if err != nil {
-			if len(msg) >= 0 {
-				panic(fmt.Sprintf("%s: %s", msg, err.Error()))
+			if len(msg) > 0 {
+				writeErr(fmt.Sprintf("%s: %s\n", msg[0], err.Error()))
 			}
-			panic(err)
+			writeErr(err.Error() + "\n")
 		}
 	}
 
@@ -41,22 +44,23 @@ func NewBuildCmd() *cobra.Command {
 
 	callback := func(cmd *cobra.Command, args []string) {
 		reader := pkg.NewDefaultConfigReader(func(s string) {
-			write(fmt.Sprintf("Parsing file: `%s`\n", s))
+			write(fmt.Sprintf("\t%s\n", s))
 		})
+		write("Reading files...\n")
 		input, err := reader.Read(inputFiles)
 		handleErr(err)
 
 		imps := imports.NewSimpleImports("_gontainer")
-		compiler := createCompiler(imps)
+		compiler := pkg.NewDefaultCompiler(imps)
 
 		compiledInput, ciErr := compiler.Compile(input)
-		handleErr(ciErr)
+		handleErr(ciErr, "Cannot build container")
 
 		tpl, tplErr := template2.NewSimpleBuilder(imps).Build(compiledInput)
 		handleErr(tplErr, "Unexpected error has occurred during building container")
 		fileErr := ioutil.WriteFile(outputFile, []byte(tpl), 0644)
 		handleErr(fileErr, "Error has occurred during saving file")
-		write(fmt.Sprintf("Container has been built [%s]\n", outputFile))
+		write(fmt.Sprintf("Container has been built\n\t%s\n", outputFile))
 	}
 
 	cmd = &cobra.Command{
@@ -73,37 +77,4 @@ func NewBuildCmd() *cobra.Command {
 	_ = cmd.MarkFlagRequired("output")
 
 	return cmd
-}
-
-func createCompiler(imports imports.Imports) *dto.BaseCompiler {
-	tokenizer := tokens.NewPatternTokenizer([]tokens.TokenFactoryStrategy{
-		tokens.NewTokenSimpleFunction(imports, "env", "os", "Getenv"),
-		tokens.NewTokenSimpleFunction(imports, "envInt", "github.com/gomponents/gontainer/pkg/std", "MustGetIntEnv"),
-		tokens.TokenPercentSign{},
-		tokens.TokenReference{},
-		tokens.TokenString{},
-	})
-
-	exporter := exporters.NewDefaultExporter()
-
-	argumentResolver := arguments.NewSimpleResolver(
-		[]arguments.Subresolver{
-			arguments.NewServiceResolver(imports, syntax.NewSimpleServiceResolver(syntax.NewSimpleTypeResolver(imports))),
-			arguments.NewPatternResolver(
-				tokenizer,
-				exporter,
-				imports,
-			),
-		},
-	)
-
-	return dto.NewBaseCompiler(
-		imports,
-		dto.NewDefaultValidator(),
-		dto.NewDefaultCompiledValidator(),
-		parameters.NewSimpleBagFactory(tokenizer, exporter, imports),
-		syntax.NewSimpleTypeResolver(imports),
-		argumentResolver,
-		syntax.NewSimpleFunctionResolver(imports),
-	)
 }
---
+++ pkg/defaults.go
@@ -0,0 +1,44 @@
+package pkg
+
+import (
+	"github.com/gomponents/gontainer/pkg/arguments"
+	"github.com/gomponents/gontainer/pkg/dto"
+	"github.com/gomponents/gontainer/pkg/exporters"
+	"github.com/gomponents/gontainer/pkg/imports"
+	"github.com/gomponents/gontainer/pkg/parameters"
+	"github.com/gomponents/gontainer/pkg/syntax"
+	"github.com/gomponents/gontainer/pkg/tokens"
+)
+
+func NewDefaultCompiler(imports imports.Imports) dto.Compiler {
+	tokenizer := tokens.NewPatternTokenizer([]tokens.TokenFactoryStrategy{
+		tokens.NewTokenSimpleFunction(imports, "env", "os", "Getenv"),
+		tokens.NewTokenSimpleFunction(imports, "envInt", "github.com/gomponents/gontainer/pkg/std", "MustGetIntEnv"),
+		tokens.TokenPercentSign{},
+		tokens.TokenReference{},
+		tokens.TokenString{},
+	})
+
+	exporter := exporters.NewDefaultExporter()
+
+	argumentResolver := arguments.NewSimpleResolver(
+		[]arguments.Subresolver{
+			arguments.NewServiceResolver(imports, syntax.NewSimpleServiceResolver(syntax.NewSimpleTypeResolver(imports))),
+			arguments.NewPatternResolver(
+				tokenizer,
+				exporter,
+				imports,
+			),
+		},
+	)
+
+	return dto.NewBaseCompiler(
+		imports,
+		dto.NewDefaultValidator(),
+		dto.NewDefaultCompiledValidator(),
+		parameters.NewSimpleBagFactory(tokenizer, exporter, imports),
+		syntax.NewSimpleTypeResolver(imports),
+		argumentResolver,
+		syntax.NewSimpleFunctionResolver(imports),
+	)
+}
