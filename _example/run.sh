4b9d2f8f9381031f21a99a2e64fbc4c84e1ceafb
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-12 03:21:39 +0400

    test

--- _example/run.sh
+++ _example/run.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ENV=dev
-go run ./main.go build -i ${DIR}/../example/example.yml -i ${DIR}/../example/params_${ENV}.yml -o ${DIR}/../example/container/example.go
-PERSON_SALARY="3500" PERSON_POSITION="Chief Technology Officer " go run ${DIR}/../example/main.go
+go run ./main.go build -i ${DIR}/../_example/example.yml -i ${DIR}/../_example/params_${ENV}.yml -o ${DIR}/../_example/container/example.go
+PERSON_SALARY="3500" PERSON_POSITION="Chief Technology Officer " go run ${DIR}/../_example/main.go
 #cat ${DIR}/../example/container/example.go
