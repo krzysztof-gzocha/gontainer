f29df00d5ac605b1033aaff3ffcb6d3a25c59666
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-12 19:13:37 +0400

    test

--- _example/run.sh
+++ _example/run.sh
@@ -6,7 +6,10 @@ ENV=dev
 cd ${DIR}/..
 go run ./main.go build -i ${DIR}/../_example/example.yml -i ${DIR}/../_example/params_${ENV}.yml -o ${DIR}/../_example/container/example.go
 cd ${DIR}
-go mod tidy & go mod vendor
+echo TIDY
+go mod tidy
+echo VENDOR
+go mod vendor
 cat go.mod
 PERSON_SALARY="3500" PERSON_POSITION="Chief Technology Officer " go run main.go
 #cat ${DIR}/../example/container/example.go
