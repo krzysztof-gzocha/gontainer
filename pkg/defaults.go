b67b9d5da10979202e021cf6eb8f3748df7b14e5
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-16 01:13:23 +0400

    test

--- pkg/defaults.go
+++ pkg/defaults.go
@@ -14,7 +14,7 @@ func NewDefaultCompiler(imports imports.Imports) dto.Compiler {
 	tokenizer := tokens.NewPatternTokenizer([]tokens.TokenFactoryStrategy{
 		tokens.NewTokenSimpleFunction(imports, "env", "os", "Getenv"),
 		tokens.NewTokenSimpleFunction(imports, "envInt", "github.com/gomponents/gontainer-helpers/env", "MustGetInt"),
-		tokens.NewTokenSimpleFunction(imports, "todo", "github.com/gomponents/gontainer-helpers/std", "MustGetMissingParameter"),
+		tokens.NewTokenSimpleFunction(imports, "todo", "github.com/gomponents/gontainer-helpers/std", "GetMissingParameter"),
 		tokens.TokenPercentSign{},
 		tokens.TokenReference{},
 		tokens.TokenString{},
