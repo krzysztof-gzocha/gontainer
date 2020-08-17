67b1d0b69e592836b100615c1d23afb9fa7c8560
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-17 20:11:00 +0400

    added `calls`

--- pkg/dto/input/dto.go
+++ pkg/dto/input/dto.go
@@ -1,16 +1,63 @@
 package input
 
 import (
+	"fmt"
 	"github.com/gomponents/gontainer/pkg/parameters"
 )
+
+// todo https://symfony.com/blog/new-in-symfony-4-3-configuring-services-with-immutable-setters
+type Call struct {
+	Method    string
+	Args      []interface{}
+	Immutable bool
+}
+
+func (c *Call) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var z []interface{}
+	if err := unmarshal(&z); err != nil {
+		return err
+	}
+	fmt.Println(z)
+
+	if len(z) == 0 || len(z) > 3 {
+		return fmt.Errorf("object Call must contain 1 - 3 args, %d given", len(z))
+	}
+
+	if s, ok := z[0].(string); !ok {
+		return fmt.Errorf("first element of object Call must be a string, `%T` given", z[0])
+	} else {
+		c.Method = s
+	}
+
+	if len(z) >= 2 {
+		if args, ok := z[1].([]interface{}); !ok {
+			return fmt.Errorf("second element of object Call must be an array, `%T` given", z[1])
+		} else {
+			c.Args = args
+		}
+	}
+
+	if len(z) >= 3 {
+		if i, ok := z[2].(bool); !ok {
+			return fmt.Errorf("third element of object Call must be a bool, `%T` given", z[2])
+		} else {
+			c.Immutable = i
+		}
+	}
+
+	return nil
+}
 
 type Service struct {
 	Getter      string   `yaml:"getter"`
 	Type        string   `yaml:"type"`
+	Value       string   `yaml:"value"`
 	Constructor string   `yaml:"constructor"`
-	Disposable  bool     `yaml:"disposable"`
 	Args        []string `yaml:"args"`
+	Calls       []Call   `yaml:"calls"`
+	Disposable  bool     `yaml:"disposable"`
 	Tags        []string `yaml:"tags"`
+	Todo        bool     `yaml:"todo"`
 }
 
 type DTO struct {
---
+++ pkg/dto/input/dto_test.go
@@ -0,0 +1,30 @@
+package input
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+	_ "github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+)
+
+func TestCall_UnmarshalYAML(t *testing.T) {
+	// todo more tests
+	y := `
+- ["SetName", ["Mary"], true]
+`
+	calls := make([]Call, 0)
+	err := yaml.Unmarshal([]byte(y), &calls)
+	assert.NoError(t, err)
+	assert.Equal(
+		t,
+		[]Call{
+			{
+				Method:    "SetName",
+				Args:      []interface{}{"Mary"},
+				Immutable: true,
+			},
+		},
+		calls,
+	)
+}
