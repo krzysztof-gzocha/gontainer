c5772704c18fd50ba31b0d029d8213b4f7d7af22
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-18 22:46:43 +0400

    consts

--- pkg/regex/consts.go
+++ pkg/regex/consts.go
@@ -1,7 +1,7 @@
 package regex
 
 const (
-	Import    = `[A-Za-z](\/?[A-Z-a-z0-9._-])*`
+	Import    = `[A-Za-z](\/?[A-Z-a-z0-9._-])*` // todo gopkg.in/yaml.v2
 	GoToken   = `[A-Za-z][A-Za-z0-9_]*`
 	YamlToken = `[A-Za-z](\.?[A-Za-z0-9_])*`
 	GoFunc    = `((?P<import>` + Import + `)\.)?(?P<fn>` + GoToken + `)`
@@ -16,8 +16,11 @@ const (
 	MetaParamName = YamlToken
 
 	MetaServiceName        = YamlToken
-	MetaServiceGetter      = MetaPkg
+	MetaServiceGetter      = GoToken
 	MetaServiceType        = `\*?` + `(` + Import + `\.)?` + GoToken
 	MetaServiceValue       = `(` + Import + `\.)?(` + GoToken + `\{\}\.)?` + GoToken
 	MetaServiceConstructor = GoFunc
+	MetaServiceCallName    = GoToken
+	MetaServiceFieldName   = GoToken
+	MetaServiceTag         = YamlToken
 )
