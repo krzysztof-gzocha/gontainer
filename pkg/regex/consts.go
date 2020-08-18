e9ed1925e066e808af8b41aa8f44785f0158e63e
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-18 22:53:50 +0400

    consts

--- pkg/regex/consts.go
+++ pkg/regex/consts.go
@@ -1,7 +1,7 @@
 package regex
 
 const (
-	Import    = `[A-Za-z](\/?[A-Z-a-z0-9._-])*` // todo gopkg.in/yaml.v2
+	Import    = `[A-Za-z](\/?[A-Z-a-z0-9._-])*`
 	GoToken   = `[A-Za-z][A-Za-z0-9_]*`
 	YamlToken = `[A-Za-z](\.?[A-Za-z0-9_])*`
 	GoFunc    = `((?P<import>` + Import + `)\.)?(?P<fn>` + GoToken + `)`
@@ -15,10 +15,14 @@ const (
 
 	MetaParamName = YamlToken
 
-	MetaServiceName        = YamlToken
-	MetaServiceGetter      = GoToken
-	MetaServiceType        = `\*?` + `(` + Import + `\.)?` + GoToken
-	MetaServiceValue       = `(` + Import + `\.)?(` + GoToken + `\{\}\.)?` + GoToken
+	MetaServiceName   = YamlToken
+	MetaServiceGetter = GoToken
+	MetaServiceType   = `\*?` + `(` + Import + `\.)?` + GoToken
+	// MetaServiceValue supports
+	// Value
+	// my/import.Value
+	// my/import.StructName{}.Method
+	MetaServiceValue       = `((?P<import>` + Import + `)\.)?((?P<struct>` + GoToken + `)\{\}\.)?(?P<value>` + GoToken + `)`
 	MetaServiceConstructor = GoFunc
 	MetaServiceCallName    = GoToken
 	MetaServiceFieldName   = GoToken
