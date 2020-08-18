4efb18e39f6d6de80c28462cfe98fb142f8fda00
Bartłomiej Krukowski <krukowski.bartlomiej@gmail.com>
2020-08-18 22:57:35 +0400

    consts

--- pkg/regex/consts.go
+++ pkg/regex/consts.go
@@ -1,10 +1,11 @@
 package regex
 
 const (
-	Import    = `[A-Za-z](\/?[A-Z-a-z0-9._-])*`
-	GoToken   = `[A-Za-z][A-Za-z0-9_]*`
-	YamlToken = `[A-Za-z](\.?[A-Za-z0-9_])*`
-	GoFunc    = `((?P<import>` + Import + `)\.)?(?P<fn>` + GoToken + `)`
+	BaseImport = `[A-Za-z](\/?[A-Z-a-z0-9._-])*`
+	Import     = `(` + BaseImport + `)|("` + BaseImport + `")`
+	GoToken    = `[A-Za-z][A-Za-z0-9_]*`
+	YamlToken  = `[A-Za-z](\.?[A-Za-z0-9_])*`
+	GoFunc     = `((?P<import>` + Import + `)\.)?(?P<fn>` + GoToken + `)`
 
 	MetaPkg           = GoToken
 	MetaContainerType = GoToken
